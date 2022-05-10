9a683a4eba94b042d209cac937347a21e8e30cf7
Darkhorse31 <prateek1234569@gmail.com>
2022-05-10 21:23:50 +0530

    Chardone

---
+++ src/dummydata.js
@@ -0,0 +1,51 @@
+const data = [
+    {
+      name: "jan",
+      "Active user": 1200,
+    },
+    {
+      name: "february",
+      "Active user": 3000,
+    },
+    {
+      name: "march",
+      "Active user": 2000,
+    },
+    {
+      name: "april",
+      "Active user": 2780,
+    },
+    {
+      name: "may",
+      "Active user": 1890,
+    },
+    {
+      name: "june",
+      "Active user": 2390,
+    },
+    {
+      name: "july",
+      "Active user": 3490,
+    },
+    {
+      name: "Aug",
+      "Active user": 1000,
+    },
+    {
+      name: "Sept",
+      "Active user": 1200,
+    },
+    {
+      name: "Oct",
+      "Active user": 2500,
+    },
+    {
+      name: "Nov",
+      "Active user": 2900,
+    },
+    {
+      name: "Des",
+      "Active user": 400,
+    },
+  ];
+  export default data;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Chart from "../../components/Charts/Chart";
 import Info from "../../components/Featuredinfo";
 import "./Home.scss";
+import Userdata from "../../dummydata"
 const Home = () => {
   return (
     <div className="home">
       <Info />
+      <Chart data={Userdata} title={"User Analytics"} grid dataKey={"Active users"}/>
     </div>
   );
 };
